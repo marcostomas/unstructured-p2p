5656684ac8dfa5a021295bde5b0a1366441b9d32
Marcos <mtomasferreira@hotmail.com>
2024-05-16 16:55:04 -0300

    mais algumas adições

--- main.go
+++ main.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net"
 	"os"
-	"io/ioutil"
+	"strings"
 )
 
-func criaSocketTCP(){
+func criaSocketTCP(endereco, porta string) {
 	// Cria um socket TCP
-	ln, err := net.Listen("tcp", ":8080")
+	ln, err := net.Listen("tcp4", endereco+":"+porta)
 	if err != nil {
 		fmt.Println("Erro ao criar socket TCP")
 		return
@@ -17,54 +18,52 @@ func criaSocketTCP(){
 	defer ln.Close()
 }
 
-func lerArquivo(nomeArquivo string) {
-    // Abre o arquivo
-    arquivo, err := os.Open(nomeArquivo)
-    if err != nil {
-        fmt.Println("Erro ao abrir o arquivo:", err)
-        return
-    }
-    defer arquivo.Close()
+func lerArquivo(nomeArquivo string) []byte {
+	// Abre o arquivo
+	arquivo, err := os.Open(nomeArquivo)
+	if err != nil {
+		fmt.Println("Erro ao abrir o arquivo:", err)
+		return nil
+	}
+	defer arquivo.Close()
 
-    // Lê o conteúdo do arquivo
-    conteudo, err := ioutil.ReadAll(arquivo)
-    if err != nil {
-        fmt.Println("Erro ao ler o arquivo:", err)
-        return
-    }
+	// Lê o conteúdo do arquivo
+	conteudo, err := ioutil.ReadAll(arquivo)
+	if err != nil {
+		fmt.Println("Erro ao ler o arquivo:", err)
+		return nil
+	}
 
-    // Imprime o conteúdo do arquivo
-    fmt.Println(string(conteudo))
-	fmt.Println("")
+	return conteudo
 }
 
-func main(){
+func main() {
 	// os.Args fornece acesso aos argumentos de linha de comando
-    args := os.Args
-
-    // os.Args[0] é o nome do programa
-    // os.Args[1:] são os argumentos que foram passados
-    fmt.Println("Nome do programa:", args[0])
-    fmt.Println("Argumentos passados:", args[1:])
-
-
+	args := os.Args
+	var endereco, porta string
 	if len(args) < 1 {
-		fmt.Println("Informe um endereço e porta para criar o socket TCP");
+		fmt.Println("Informe um endereço e porta para criar o socket TCP")
 	} else if len(args) == 2 {
-		fmt.Println("Endereço e porta informados: ", args[1]);
-	} else if len(args) == 4{
-		fmt.Println(fmt.Sprintf("Endereço e porta informados: %s \n", args[1]));
+		fmt.Println("Endereço e porta informados: ", args[1])
+		enderecoCompleto := strings.Split(args[1], ":")
+		endereco, porta = enderecoCompleto[0], enderecoCompleto[1]
 
-		fmt.Println("Arquivo de vizinhos:");
-		lerArquivo(args[2]);
+	} else if len(args) == 4 {
+		fmt.Println(fmt.Sprintf("Endereço e porta informados: %s \n", args[1]))
 
-		fmt.Println("Arquivo de pares chave-valor:");
-		lerArquivo(args[3]);
+		vizinhosRaw := lerArquivo(args[2])
+		vizinhos := strings.Split(string(vizinhosRaw), "\n")
+
+		fmt.Println("Vizinhos: ", vizinhos)
+
+		paresRaw := lerArquivo(args[3])
+		pares := strings.Split(string(paresRaw), "\n")
+
+		fmt.Println("pares:", pares)
 	} else {
-		fmt.Println("Número de argumentos inválido");
+		fmt.Println("Número de argumentos inválido")
 		return
 	}
 
-	// lerTerminal()
-	// criaSocketTCP()
+	criaSocketTCP(endereco, porta)
 }
