5aaf34d9eb8d11bb6cefda837ac1dd9ffefbafd6
rafael2051 <rafael.moura.almeida@usp.br>
2024-06-10 23:46:13 -0300

    some changes

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-cd ../src;
-go install UP2P;
-cd client; go install UP2P/client;
-cd ../server; go install UP2P/server;
-cd ../node; go install UP2P/node;
-cd ..; go build;
-cd ../scripts;
+sudo apt install go
+cd ..
+cd src
+go build
